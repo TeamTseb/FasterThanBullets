d5df02a46f0c692f6c7325a96724ab9c3fbdcd42
Hedgeriot <john.mace@ucc.on.ca>
2016-03-24 14:06:48 -0400

    More things happening...
    First of the improvements: the canvas enters itself automatically and
    no longer assumes you’re using the window size I was using, so that’s
    an improvement. I haven’t tested anything yet but it looks good. I’m
    going to a party tonight but I already know it’s gonna be pretty
    boring. I’ll work on this and push whatever I can tomorrow by 12:00 AM
    Saturday. Wish me luck.

--- allrightnow.js
+++ allrightnow.js
@@ -1,6 +1,33 @@
+// DEATHPONG! 
+// IT'S LIKE LETHAL LEAGUE BUT IT'S WORSE
 
-var HEIGHT = 625;
-var WIDTH = 1235;
+//When refreing to the construction of 
+
+var canvas = document.getElementById("gamecanvas");
+var ctx = canvas.getContext("2d");
+var oneHeight = 80;
+var oneWidth = 10;
+var twoHeight = 80;
+var twoWidth = 10;
+
+var black = "#000000";
+
+var Initial_Y_Left = (canvas.width-oneWidth)/2;
+var Initial_Y_Right = (canvas.width-twoWidth)/2;
+var oneDisplacement = 5;
+var twodisplacement = 5;
+
+var W_Up = false;
+var S_Down = false;
+var I_Up = false;
+var K_Down = false;
+
+function leftPaddle() {
+	ctx.beginPath();
+	ctx.rect(Initial_Y_Left, canvas.height-oneHeight, oneWidth, oneHeight);
+	ctx.fillStyle = "#000000" //Fills black
+	ctx.closePath();
+}
 
 /*
 ###########################+++++###########
