eca37a30f7c4060872ed627b3f0c23eb19c967bb
Hedgeriot <john.mace@ucc.on.ca>
2016-03-28 15:52:59 -0400

    Added images and sound. Also added more to game file
    Main file is roughly 1 sixth complete. Engine file still needs to be
    dome as well

--- deathpong.js
+++ deathpong.js
@@ -12,112 +12,186 @@
 //---------------------------------------------------------------------------------------------------
 
 
-//Canvas width is 1235, canvas height is 625
-var canvas = document.getElementById("gamecanvas");
-var ctx = canvas.getContext("2d");
-var oneHeight = 80;
-var oneWidth = 10;
-var twoHeight = 80;
-var twoWidth = 10;
+Deathpong {
 
-var black = "#000000";
+	Defaults: {
+		width: 640,
+		height: 480,
+		wallWidth: 12,
+		paddleWidth: 12,
+		paddleHeight: 60,
+		paddleSpeed: 2,
+		ballSpeed: 4,
+		ballAcceleration: 8,
+		ballRadius: 5,
+		sound: true
+	}, //Commas are here as these are within the Deathpong bracket
 
-var Initial_X_Left = (canvas.width)-(canvas.width + 5)
-var Initial_X_Right = (canvas.width)- 5;
-var Initial_Y_Left = (canvas.height)/2;
-var Initial_Y_Right = (canvas.height)/2;
-var oneDisplacement = 5;
-var twodisplacement = 5;
+	Colours: {
+		walls: 'white',
+		ball: 'white',
+		score: 'white',
+		footprint: '#333',
+		predictGuess: 'yellow',
+		predictExact: 'red'
+	},
 
-var W_Up = false;
-var S_Down = false;
-var U_Arrow_Up = false;
-var S_Arrow_Down = false;
+	Images: [ //String array of images used
+		"IMAGES/press1.png",
+		"IMAGES/press2.png",
+		'IMAGES/winner.png'
+	],
 
-function leftPaddle() {
-	ctx.beginPath();
-	//Rectangle construction properties are height, width, X position, Y position
-	ctx.rect(oneHeight, oneWidth, , Initial_X_Left, Initial_Y_Left);
-	ctx.fillStyle = "#000000" //Fills black
-	ctx.fill();
-	ctx.closePath();
+	Levels: [
+	    {aiReaction: 0.2, aiError:  40}, // 0:  ai is losing by 8
+	    {aiReaction: 0.3, aiError:  50}, // 1:  ai is losing by 7
+	    {aiReaction: 0.4, aiError:  60}, // 2:  ai is losing by 6
+	    {aiReaction: 0.5, aiError:  70}, // 3:  ai is losing by 5
+	    {aiReaction: 0.6, aiError:  80}, // 4:  ai is losing by 4
+	    {aiReaction: 0.7, aiError:  90}, // 5:  ai is losing by 3
+	    {aiReaction: 0.8, aiError: 100}, // 6:  ai is losing by 2
+	    {aiReaction: 0.9, aiError: 110}, // 7:  ai is losing by 1
+	    {aiReaction: 1.0, aiError: 120}, // 8:  tie
+	    {aiReaction: 1.1, aiError: 130}, // 9:  ai is winning by 1
+	    {aiReaction: 1.2, aiError: 140}, // 10: ai is winning by 2
+	    {aiReaction: 1.3, aiError: 150}, // 11: ai is winning by 3
+	    {aiReaction: 1.4, aiError: 160}, // 12: ai is winning by 4
+	    {aiReaction: 1.5, aiError: 170}, // 13: ai is winning by 5
+	    {aiReaction: 1.6, aiError: 180}, // 14: ai is winning by 6
+	    {aiReaction: 1.7, aiError: 190}, // 15: ai is winning by 7
+	    {aiReaction: 1.8, aiError: 200}  // 16: ai is winning by 8
+  ],
+
+	initialize: function(runner, cfg) {
+		game.loadImages(Deathpong.IMAGES, function(Images) {
+			this.cfg = cfg;
+			this.runner = runner;
+			this.width = runner.width;
+			this.height = runner.height;
+			//Check naming convention. I'm fuzzy on these capital letters
+			this.images = images;
+			//The playeris not playing by default.
+			this.playing = false;
+			this.scores = [0, 0];
+			this.menu = Object.construct(Deathpong.Menu, this);
+			this.court = Object.coustruct(Deathpong.Court, this);
+			this.leftPaddle = Object.coustruct(Deathpong.Paddle, this);
+			this.rightPaddle = Object.coustruct(Deathpong.Paddle, this, true);
+			this.ball = Object.construct(Deathpong.Ball, this);	
+			this.sounds = Object.coustruct(Deathpong.Sounds, this);
+			this.runner.start();
+		}.bind(this)); //Second close parenthesses is from game.loadImages line
+	},   
+
+	startDemo: function() { this.start(0); },
+	startSinglePlayer: function() { this.start(1); },
+	startTwoPlayer: function () { this.start(2); },
+
+	start: function(numPlayers) {
+		if (!this.playing) {
+			this.scores = [0, 0];
+			this.playing = true;
+			this.leftPaddle.setAuto(numPlayers < 1, this.level(0));
+			this.rightPaddle.setAuto(numPlayers < 2, this.level(1));
+			this.ball.reset(); 
+			this.runner.hideCursor();
+		}
+	},
+
+	stop: function(ask) {
+		if (this.playing) {
+			//Send a JavaScript prompt asking the user if they want to quit their game
+			if (!ask || this.runenr.confirm('Quit current game?')) {
+				this.playing = false;
+				this.leftPaddle.setAuto(false);
+				this.rightPaddle.setAuto(false);
+				this.runner.showCursor();
+			}
+		}
+	},
+
+	level: function(playerNo) {
+		return 8 + (this.scores[playerNo] - this.scores[playerNo ? 0 : 1]); 
+	}
+
+	goal: function(playerNo) {
+		this.sounds.goal();
+		this.scores[playerNo] += 1;
+		if (this.scores[playerNo] == 9) {
+			this.menu.declareWinner(playerNo);
+			this.stop();
+		}
+		else {
+			this.ball.reset(playerNo);
+			this.leftPaddle.setLevel(this.level(0));
+			this.rightPaddle.setLevel(this.level(1));
+		}
+	},
+
+	update: function(dt) {
+		this.leftPaddle.update(dt, this.ball);
+		this.rightPaddle.update(dt, this.ball);
+		if (this.playing) {
+			var dx = this.ball.dx;
+			var dy = this.ball.dy;
+			this.ball.update(dt, this.leftPaddle, this.rightPaddle);
+			if (this.ball.dx < 0 && dx > 0)
+				this.sounds.ping(); 
+			else if (this.ball.dx > 0 && dx < 0)
+				this.sounds.pong();
+			else if (this.ball.dy * dy < 0)
+				this.sounds.wall();
+
+			if (this.ball.left > this.width)
+				this.goal(0);
+			else if (this.ball.right < 0)
+				this.goal(1);
+		}
+	},
+
+	draw: function(ctx) {
+		this.court.draw(ctx, this.scores[0], this.scores[1]);
+		this.leftPaddle.draw(ctx);
+		this.rightPaddle.draw(ctx);
+		if (this.playing)
+			this.ball.draw(ctx);
+		else
+			this.menu.draw(ctx);
+	},
+
+	onkeydown: function(keyCode) {
+		switch(keyCode) {
+			case Game.KEY.ZERO: this.startDemo();            break;
+			case Game.KEY.ONE:  this.startSinglePlayer();    break;
+	        case Game.KEY.TWO:  this.startTwoPlayer();       break;
+   	        case Game.KEY.ESC:  this.stop(true);             break;
+		    case Game.KEY.Q:    if (!this.leftPaddle.auto)  this.leftPaddle.moveUp();    break; //Change to W
+		    case Game.KEY.A:    if (!this.leftPaddle.auto)  this.leftPaddle.moveDown();  break; //Change to S
+		    case Game.KEY.P:    if (!this.rightPaddle.auto) this.rightPaddle.moveUp();   break; //Change to Up arrow or I
+ 		    case Game.KEY.L:    if (!this.rightPaddle.auto) this.rightPaddle.moveDown(); break; //Change to Down arrow or K
+		}
+	},
+
+	onkeyup: function(keycode) {
+		switch(keyCode) {
+	        case Game.KEY.Q: if (!this.leftPaddle.auto)  this.leftPaddle.stopMovingUp();    break;
+	        case Game.KEY.A: if (!this.leftPaddle.auto)  this.leftPaddle.stopMovingDown();  break;
+	        case Game.KEY.P: if (!this.rightPaddle.auto) this.rightPaddle.stopMovingUp();   break;
+	        case Game.KEY.L: if (!this.rightPaddle.auto) this.rightPaddle.stopMovingDown(); break;
+		}
+
+	},
+
+	Menu: {
+		initialize: function(Deathpong) {
+			var press1 = Deathpong.IMAGES["IMAGES/press1.png"];
+			var press2 = Deathpong.IMAGES["IMAGES/press2.png"];
+			var winner = Deathpong.IMAGES["IMAGES/winner.png"];
+		}
+	}
+
 }
 
-function rightPaddle() {
-	ctx.beginPath();
-	ctx.rect(twoHeight, twoWidth, Initial_X_Right, Initial_Y_Right);
-	ctx.fillStyle = "#000000"
-	ctx.fill();
-	ctx.closePath();
-}
-
-
-// W is 87 (Paddle one up)
-// S is 83 (Paddle one down)
-// Up arrow is 38 (Paddle two up)
-// Down arrow is 40 (Paddle two down)
-
-function keyPressed(e) {
-	if (e.keyCode == 87) {
-		W_Up = true;
-	}
-	if (e.keyCode == 83) {
-		S_Down = true;
-	}
-	if (e.keyCode == 38) {
-		U_Arrow_Up = true;
-	}
-	if (e.keyCode == 40) {
-		S_Arrow_Down = true;
-	}
-}
-
-function keyReleased(e) {
-	if (e.keyCode == 87) {
-		W_Up = false;
-	}
-		if (e.keyCode == 83) {
-		S_Down = false;
-	}
-	if (e.keyCode == 38) {
-		U_Arrow_Up = false;
-	}
-	if (e.keyCode == 40) {
-		D_Arrow_Down = false;
-	}
-}
-
-function moveLeft_Paddle() {
-	if (W_Up && Initial_Y_Left < 920) {
-		Initial_Y_Left += oneDisplacement;
-	}
-	else if (S_Down && Initial_Y_Left > 0) {
-		Initial_Y_Left -= oneDisplacement;
-	}
-}
-
-function moveRight_Paddle() {
-	if (U_Arrow_Up && Initial_Y_Right < 920) {
-		Initial_Y_Right += twodisplacement;
-	}
-	else if (D_Arrow_Down && Initial_Y_Right > 0) {
-		Initial_Y_Right -= twodisplacement;
-	}
-}
-
-
-function draw_paddles() {
-	ctx.clearRect(0, 0, canvas.height, canvas.width);
-	leftPaddle();
-	rightPaddle();
-}
-function draw() {
-	draw_paddles();
-}
-
-window.addEventListener("keydown", KeyPressed,true);
-window.addEventListener("keyup", KeyReleased,true);
-setInterval(draw, 20);
 
 /*
 ###########################+++++###########
